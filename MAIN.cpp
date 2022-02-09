5900bd149b3f7704fa24d262e45c088f3424c7aa
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-02-09 17:43:02 +0530

    plan

--- MAIN.cpp
+++ MAIN.cpp
@@ -4,6 +4,14 @@ class bill{
     int id;
     
 }
+//1 class bill --- 11th 5-7
+//2 list product --- minor
+//3 print menu --- minor
+//4 place order --- major
+//5 queue --- major
+//6 histogram(prefed time) 
+//7 generate and print bill
+//
 int main(){
 
     return 0;
