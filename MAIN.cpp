1607dd69e5ea640cbb78d25fdc0c473dcae34cb4
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2022-02-11 17:15:50 +0530

    Co-authored-by: Ankesh Pandey <anvaishy@users.noreply.github.com> Co-authored-by: ArkiGhosh <ArkiGhosh@users.noreply.github.com>

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
+#define int long long
+
+
+
+
 using namespace std;
-class bill{
-    int id;
+class bill
+{
+    int customerId;
+    string customerName;
+    string customerContact;
+    int totalPrice;
     
-}
-//1 class bill --- 11th 5-7
+
+};
+//1 class bill --- 11th 5-7 --- {Bill id, customer name, customer contact, product list(product name, product quantity), total price.}
 //2 list product --- minor
 //3 print menu --- minor
 //4 place order --- major
@@ -12,7 +22,8 @@ class bill{
 //6 histogram(prefed time) 
 //7 generate and print bill
 //
-int main(){
+int main()
+{
 
     return 0;
 }
